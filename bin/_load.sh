0646e18f5def514f88b649132cceef09ba92b57d
maggie-5miles <maggie@wespoke.com>
2017-03-28 17:36:41 +0800

    skip no change records, only do full load on Sunday

--- bin/_load.sh
+++ bin/_load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
   echo "No arguments supplied. "
-  echo 'Usage : Script <process_date> <file_name> <list_id> [<batch_size> <skip>]'
+  echo 'Usage : Script <process_date> <file_name> <list_id> [<batch_size>]'
   exit 1
 fi
 
@@ -12,9 +12,13 @@ list_id=$3
 if [ "$4" != "" ]; then
   batch_size="-b $4"
 fi
-if [ "$5" != "" ]; then
+
+# skip no change records, only do full load on Sunday
+w=`date +%w`
+if [ "$w" != "0" ]; then
   skip="-s"
 fi
+
 # set -x
 java -Dlog4j.configurationFile=./log4j2.xml -DdateDir=$date_dir -DuserListFile=$file_name -jar target/bulk-uploader-1.0.jar \
   -f /fivemiles/scripts/bulk-uploader/s3data/$process_date/$file_name -l $list_id $batch_size $skip
